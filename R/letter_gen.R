ab8d68868a940841723405d1a5817069acf9ced7
ritchie1 <38797594+ritchie1@users.noreply.github.com>
2018-04-27 16:46:51 -0500

    Initial commit

---
+++ R/letter_gen.R
@@ -0,0 +1,5 @@
+letter_gen <-
+function(x){
+  choose_letter <- sample(c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"),1)
+  return(choose_letter)
+}
